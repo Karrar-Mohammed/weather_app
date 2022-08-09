8312ff761663f0663dd18e953506ce3cd07aba01
Ameer Amjed <45900975+AmeerAmjed@users.noreply.github.com>
2022-08-09 14:20:13 +0300

    Feature/code clean up (#2)
    * create client class as the data source
    
    * improvement code

---
+++ app/src/main/java/com/karrar/weather_app/data/remote/Url.kt
@@ -0,0 +1,18 @@
+package com.karrar.weather_app.data.remote
+
+import com.karrar.weather_app.util.Constants
+import okhttp3.HttpUrl
+
+object Url {
+     fun getWeatherUrl(): HttpUrl {
+        return HttpUrl.Builder()
+            .scheme(Constants.Api.PROTOCOL)
+            .host(Constants.Api.HOST)
+            .addPathSegments("data/2.5/onecall")
+            .addQueryParameter(Constants.Api.LAT, "41.034283")
+            .addQueryParameter(Constants.Api.LON, "28.680119")
+            .addQueryParameter(Constants.Api.APPID, Constants.Api.KEY)
+            .addQueryParameter(Constants.Api.UNITS, "metric")
+            .build()
+    }
+}
