27667611561b5a004bc95633cf4be8793a5eedf6
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-08-10 00:37:04 +0300

    refactor main activity code

--- app/src/main/java/com/karrar/weather_app/ui/MainActivity.kt
+++ app/src/main/java/com/karrar/weather_app/ui/MainActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.karrar.weather_app.R
+import com.karrar.weather_app.data.domain.Current
+import com.karrar.weather_app.data.domain.Daily
+import com.karrar.weather_app.data.domain.Hourly
 import com.karrar.weather_app.data.domain.WeatherModel
 import com.karrar.weather_app.data.network.Client
 import com.karrar.weather_app.databinding.ActivityMainBinding
@@ -19,65 +22,80 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setup()
-    }
 
-    private fun setup() {
         getWeatherInfo()
-
     }
-
 
     private fun getWeatherInfo() {
         val observable = Observable.create { emitter ->
             emitter.onNext(State.Loading)
             emitter.onNext(Client.getWeatherData())
         }
+        subscribeOnWeatherObservable(observable)
+
+    }
+
+    private fun subscribeOnWeatherObservable(observable: Observable<State<WeatherModel?>>) {
         observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
             { response ->
-                when(response){
-                    State.Loading -> {
-//                        TODO()
-                    }
+                when (response) {
+                    State.Loading -> {}
                     is State.Success -> bindDataToUi(response.data)
-                    is State.Error -> Toast.makeText(this, response.message, Toast.LENGTH_SHORT).show()
+                    is State.Error -> Toast.makeText(this, response.message, Toast.LENGTH_SHORT)
+                        .show()
                 }
-        },{
-            Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+            }, {
+                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
             })
     }
 
+
     private fun bindDataToUi(weather: WeatherModel?) {
+        setupCurrentDayWeather(weather?.current)
+        setupDailyRecycler(weather?.daily)
+        setupHourlyRecycler(weather?.hourly)
+    }
+
+    private fun setupCurrentDayWeather(currentWeather: Current?) {
         binding.apply {
             textTemperature.text =
-                getString(R.string.temperature, weather?.current?.temp?.toInt().toString())
+                getString(R.string.temperature, currentWeather?.temp?.toInt().toString())
+
             textTimezone.text = getString(R.string.esenyurt)
             textTimezoneHeader.text = getString(R.string.esenyurt)
             textFeelsLike.text =
-                getString(R.string.feels_like, weather?.current?.feelsLike?.toInt().toString())
-            textSunriseTime.text = weather?.current?.sunrise?.formatDate(Constants.DateFormat.HOUR)
-            textSunsetTime.text = weather?.current?.sunset?.formatDate(Constants.DateFormat.HOUR)
-            imageIcon.loadWeatherIcon(weather?.current?.weatherStatus?.get(0))
+                getString(R.string.feels_like, currentWeather?.feelsLike?.toInt().toString())
 
-            weather?.hourly?.let {
-                val list = it.take(24)
-                val adapter = HourlyAdapter(list)
-                recyclerHourly.adapter = adapter
-            }
+            textSunriseTime.text = currentWeather?.sunrise?.formatDate(Constants.DateFormat.HOUR)
+            textSunsetTime.text = currentWeather?.sunset?.formatDate(Constants.DateFormat.HOUR)
+            imageIcon.loadWeatherIcon(currentWeather?.weatherStatus?.get(0))
+        }
+    }
 
-            weather?.daily?.let {
-                val list = it.take(7)
+    private fun setupDailyRecycler(dailyWeather: List<Daily>?) {
+        binding.apply {
+            dailyWeather?.let {
+                val list = dailyWeather.take(7)
                 val adapter = DailyAdapter(list)
                 recyclerDaily.adapter = adapter
                 recyclerDaily.suppressLayout(true)
             }
-
         }
     }
 
+    private fun setupHourlyRecycler(hourlyWeather: List<Hourly>?) {
+        binding.apply {
+            hourlyWeather?.let {
+                val list = hourlyWeather.take(24)
+                val adapter = HourlyAdapter(list)
+                recyclerHourly.adapter = adapter
+            }
+        }
+    }
 }
